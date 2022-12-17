c33265f184be7c0118dbd35bc4613cd04631dcd9
Maria Riosdelgado <mariariosdelgado@Marias-iMac.local>
2022-12-16 21:29:35 -0500

    button functions

--- controllers/places.js
+++ controllers/places.js
@@ -53,9 +53,25 @@ router.get('/:id', (req, res) => {
       res.render('error404')
     }
     else {
-      res.render('places/show', { place: places[id] })
+      res.render('places/show', { place: places[id], id })
     }
   })
+
+  //Delete
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
+  
   
   
 module.exports = router
