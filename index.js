5cbe5902b8c580e2f3e8e28b0ee71c275f47881e
Maria Riosdelgado <mariariosdelgado@Marias-iMac.local>
2022-12-16 19:38:28 -0500

    added a bit of styling

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Express settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+//Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
